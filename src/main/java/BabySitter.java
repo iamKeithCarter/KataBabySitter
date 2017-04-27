d1f1bb3d55541de5b621413bc74be6634f054026
Administrator <Administrator@Lomanstop.local>
2017-04-27 12:46:45 -0400

    refactored start end and bedtime method names

--- src/main/java/BabySitter.java
+++ src/main/java/BabySitter.java
@@ -3,17 +3,17 @@ public class BabySitter {
 
 	private int startTime;
 	
-	public int determineStartTime(int startTime) {
+	public int setStartTime(int startTime) {
 		this.startTime =startTime;
 				return startTime;
 	}
 
-	public int determineEndTime(int endTime) {
+	public int setEndTime(int endTime) {
 		// TODO Auto-generated method stub
 		return endTime; 
 	}
 
-	public Object determineBedTime(int bedTime) {
+	public Object setBedTime(int bedTime) {
 		// TODO Auto-generated method stub
 		return 8;
 	}
--- src/test/java/BabySitterTest.java
+++ src/test/java/BabySitterTest.java
@@ -7,27 +7,27 @@ public class BabySitterTest {
 	@Test
 	public void whenSitterPassedStartTimeItReturnsThatTime() {
 	BabySitter underTest = new BabySitter();
-	assertEquals(6,underTest.determineStartTime(6));
+	assertEquals(6,underTest.setStartTime(6));
 	}
 	
 	@Test
 	public void whenSitterPassedAnyStartTimeItReturnsThatTime() {
 	BabySitter underTest = new BabySitter();
-	assertEquals(3,underTest.determineStartTime(3));
-	assertEquals(11,underTest.determineStartTime(11));
+	assertEquals(3,underTest.setStartTime(3));
+	assertEquals(11,underTest.setStartTime(11));
 	}
 	
 	@Test
 	public void whenSitterPassedAnyEndTimeItReturnsThatTime() {
 	BabySitter underTest = new BabySitter();
-	assertEquals(4,underTest.determineEndTime(4));
-	assertEquals(7,underTest.determineEndTime(7));
+	assertEquals(4,underTest.setEndTime(4));
+	assertEquals(7,underTest.setEndTime(7));
 	}
 	
 	@Test
 	public void whenSitterPassedBedTimeItReturnsThatTime() {
 	BabySitter underTest = new BabySitter();
-	assertEquals(8,underTest.determineBedTime(8));
+	assertEquals(8,underTest.setBedTime(8));
 	
 	}
 	
